0ec52c129b5b9244e6d964eb6d4367c39e74a93d
Mohit Pandey <mp7698921@gmail.com>
2025-12-26 21:57:28 +0530

    adminviewbook fix

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from home.models import Notification
 
 # Register your models here.
 
-admin.site.register(Notification)
+@admin.register(Notification)
+class NotificationAdmin(ModelAdmin):
+    list_display = ('recipient', 'actor','notification_type')
+    notification_type = ('book__title',)
